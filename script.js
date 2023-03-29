651bb961bb606ba7d53b135980712772a322902a
unknown <edtread3@gmail.com>
2023-03-29 12:38:21 -0400

    added timeblocks for each hour up till 5pm. add and remove class based on current time, save time and text to localStorage.

--- script.js
+++ script.js
@@ -9,12 +9,11 @@ $(document).ready(function () {
     $(".saveBtn").on("click", function () {
       var text = $(this).siblings("description").val();
       var time = $(this).parent().attr("id");
+
    
-      // setting items text, and time to localStorage
-      localStorage.setItem(text, time);
+   
 
     });
-    
     
      function timeCompare() {
     
@@ -22,10 +21,10 @@ $(document).ready(function () {
     
      // create variable that displays current hour
     var currentTime = parseInt(dayjs().format("H"));
-    var hourBlock = parseInt($(this).attr("id").split("hour")[1]);
-    
+    var hourBlock = parseInt($(this).attr("id").split("-")[1]);
+ 
     // if statement that would compare hours selected to current hour set as past , present , or future
-    console.log(currentTime);
+
 
      if (hourBlock == currentTime) {
         $(this).addClass("present");
@@ -52,20 +51,22 @@ $(document).ready(function () {
  timeCompare ();
 });     
 
+// setting text, and time to localStorage
 
  // setting work day times to get from localStorage
-$("#hour-9.description").val(localStorage.getItem("hour9"));
-$("#hour-10.description").val(localStorage.getItem("hour10"));
-$("#hour-11.description").val(localStorage.getItem("hour11"));
-$("#hour-12.description").val(localStorage.getItem("hour12"));
-$("#hour-13.description").val(localStorage.getItem("hour1"));
-$("#hour-14.description").val(localStorage.getItem("hour2"));
-$("#hour-15.description").val(localStorage.getItem("hour3"));
-$("#hour-16.description").val(localStorage.getItem("hour4"));
-$("#hour-17.description").val(localStorage.getItem("hour5"));
 
-//timeCompare();
+
+for (i = 9; i <= 17; i++) {
+   $("#hour-" + i +".description").val(localStorage.getItem("hour" + i));
+ 
+}
     
+function saveDescription() {
+   const hour = $(this).parent().attr('id').split("-")[1];
+   const text = $(this).siblings(".description").val();
+   console.log(text)
+   localStorage.setItem(hour, text)
 
+}
 
-
+$(".saveBtn").click(saveDescription)
